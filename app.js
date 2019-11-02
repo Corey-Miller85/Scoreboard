abbd323d14c7f759efc381a5e3d65105b1174944
Corey Miller <coreyMiller353@gmail.com>
2019-11-01 21:55:17 -0400

    add counter functions to plus and minus buttons

--- app.js
+++ app.js
@@ -45,19 +45,42 @@ const Player = props => {
 	return (
 		<div className="player">
 			<span className="player-name">{props.name}</span>
-			<Counter score={props.score} />
+			<Counter />
 		</div>
 	);
 };
 
-const Counter = props => {
-	return (
-		<div className="counter">
-			<button className="counter-action decrement"> - </button>
-			<span className="counter-score">{props.score}</span>
-			<button className="counter-action increment"> + </button>
-		</div>
-	);
+class Counter extends React.Component {
+
+  state = {
+    score: 0
+  };
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  }
+
+  decrementScore = () => {
+    if (this.state.score <= 0) {
+      console.log('score is <= 0')
+    } else {
+      this.setState({
+        score: this.state.score - 1 
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={ this.decrementScore }> - </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment" onClick={ this.incrementScore }> + </button>
+      </div>
+    );
+  }
 };
 
 const App = props => {
@@ -68,9 +91,8 @@ const App = props => {
 			{/* PLAYERS LIST */}
 			{props.intialPlayers.map(player => (
 				<Player 
-        name={player.name} 
-        score={player.score}
-        key={player.id.toString()} 
+          name={player.name} 
+          key={player.id.toString()} 
         />
 			))}
 		</div>
